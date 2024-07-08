4b157110f9ee66a686ba7794e1945bedf7bab8e7
Saniya Mulla <140803877+Saniya-mulla03@users.noreply.github.com>
2024-07-08 19:58:27 +0530

    Update Contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -40,7 +40,7 @@ export const Contact = () => {
         e.preventDefault();
         
         try {
-            const response = await fetch("http://localhost:5000/api/form/contact", {
+            const response = await fetch("https://campus-info-server.onrender.com", {
                 method: "POST",
                 headers: {
                     'Content-Type':"application/json"
