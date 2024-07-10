a125cded35e100298b6452b854a345958c2b3339
Saniya Mulla <140803877+Saniya-mulla03@users.noreply.github.com>
2024-07-10 19:52:57 +0530

    Update CampusInfo.jsx

--- client/src/pages/CampusInfo.jsx
+++ client/src/pages/CampusInfo.jsx
@@ -90,8 +90,8 @@ export const CampusInfo = () => {
                                 </div>
                                 {isAdminLoggedIn &&
                                             <ul>
-                                                <li><button onClick={() => deleteCampusInfo(curElem._id)} style={{ marginLeft: '90px', marginBottom: '10px' }}>Delete</button></li>
-                                                <li><NavLink to={`/admin/campusInfo/${curElem._id}/edit`} style={{marginBottom: '10px'}}>Edit</NavLink></li>
+                                                <li><button onClick={() => deleteCampusInfo(curElem._id)} style={{ marginLeft: '20px', marginBottom: '10px' }}>Delete</button></li>
+                                                <li><NavLink to={`/admin/campusInfo/${curElem._id}/edit`} style={{ marginLeft: '170px',marginBottom: '10px'}}>Edit</NavLink></li>
                                             </ul>
                                         }
                             </div>
