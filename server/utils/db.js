4b9ac2f066a08ada2646581eeb69ef5c26529055
Saniya Mulla <140803877+Saniya-mulla03@users.noreply.github.com>
2025-02-10 17:51:49 +0530

    Update db.js

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-//const URI = "mongodb://localhost:27017/mern_react_admin";
-// mongoose.connect(URI);
-//const URI = "mongodb+srv://saniya:BQxTcgjTzzi9XhjR@cluster0.0bcs0rp.mongodb.net/mern_admin?retryWrites=true&w=majority&appName=Cluster0";
 const URI = process.env.MONGODB_URI;
 
 const connectDB = async() => {
